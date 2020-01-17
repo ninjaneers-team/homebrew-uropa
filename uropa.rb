a286e3d36ebf96a09958b30c9a696ec19b77a36f
Ninjaneers GmbH <info@ninjaneers.de>
2020-01-17 16:53:56 +0100

    Brew formula update for uropa version v1.3.0

--- uropa.rb
+++ uropa.rb
@@ -2,16 +2,16 @@
 class Uropa < Formula
   desc "Declarative configuration for Opa"
   homepage "https://github.com/ninjaneers-team/uropa"
-  version "1.1.9"
+  version "1.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ninjaneers-team/uropa/releases/download/v1.1.9/uropa_1.1.9_darwin_amd64.tar.gz"
-    sha256 "e7e2bff06bbddb285e1f53762eeace385a8b8b302f69bc1e04afb9b7702b06d6"
+    url "https://github.com/ninjaneers-team/uropa/releases/download/v1.3.0/uropa_1.3.0_darwin_amd64.tar.gz"
+    sha256 "4216809e66b2734c67496539fd7bbf9013aaaf3c21f4fe85936ee83a23e80269"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ninjaneers-team/uropa/releases/download/v1.1.9/uropa_1.1.9_linux_amd64.tar.gz"
-      sha256 "f7d54d3ee235a59cdbe964b6d2082cf641d0cb5b52e60c10f55333666126d1f2"
+      url "https://github.com/ninjaneers-team/uropa/releases/download/v1.3.0/uropa_1.3.0_linux_amd64.tar.gz"
+      sha256 "94866547bc98e64b0075946e13d88f012566399cba288bf2ea45b0005d1f861f"
     end
   end
 
